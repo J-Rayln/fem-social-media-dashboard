911775017f3aee00abf4c88c777ab704542a1cac
J-Rayln <103427109+J-Rayln@users.noreply.github.com>
2022-06-29 15:09:22 -0700

    Finished theme switcher

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,28 @@
-// console.log('testing the JavaScript file!');
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
 
-// if (window.CSS && CSS.supports("color", "var(--primary)")) {
-//     var toggleColorMode = function toggleColorMode(e) {
-//       // Switch to Light Mode
-//       if (e.currentTarget.classList.contains("light--hidden")) {
-//         // Sets the custom html attribute
-//         document.documentElement.setAttribute("color-mode", "light"); // Sets the user's preference in local storage
+// get the current theme and set it to dark
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
   
-//         localStorage.setItem("color-mode", "light");
-//         return;
-//       }
-//       /* Switch to Dark Mode
-//       Sets the custom html attribute */
-//       document.documentElement.setAttribute("color-mode", "dark"); // Sets the user's preference in local storage
-  
-//       localStorage.setItem("color-mode", "dark");
-//     }; // Get the buttons in the DOM
-  
-//     // var toggleColorButtons = document.querySelectorAll(".color-mode__btn"); // Set up event listeners
-//     var toggleColorButtons = document.querySelectorAll(".theme-toggle__checkbox"); // Set up event listeners
-    
-//     toggleColorButtons.forEach(function(btn) {
-//         // btn.addEventListener("click", toggleColorMode);
-//         btn.addEventListener("change", toggleColorMode);
-//     });
+    // check if the current theme is dark & set the starting toggle position
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 
-//   } else {
-//     // If the feature isn't supported, then we hide the toggle buttons
-//     var btnContainer = document.querySelector(".color-mode__header");
-//     btnContainer.style.display = "none";
-//   }
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        // set the teme into local storage
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        // se the theme into local storage
+        localStorage.setItem('theme', 'light');
+    }    
+}
 
-const checkbox = document.getElementById("theme-toggle")
-
-checkbox.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  document.documentElement.setAttribute("color-mode", "dark");
-})
-
-  
+toggleSwitch.addEventListener('change', switchTheme, false);
